91e6c4b06e7dae492f7b2a01d38c6ab343ddc17f
AeR0 <prithvichavan56110@gmail.com>
2024-08-23 23:39:37 +0530

    modularized the loop that sends the data over the tcp connection periodically

---
+++ TransmitData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using s_init;
+
+public class TransmitData
+{
+	public TransmitData()
+	{
+	}
+
+    static void TransmitData()
+    {
+        while (_exec)
+        {
+            _writer.WriteLine(valJson);
+            Console.WriteLine("Sensor Data sent!");
+            Thread.sleep(1000);
+        }
+    }
+}
