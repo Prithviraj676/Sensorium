3b5af099f00ceae55a3d84082eee00bf51ee5691
AeR0 <prithvichavan56110@gmail.com>
2024-12-08 16:24:13 +0530

    caching these files

--- ServerInit.cs
+++ ServerInit.cs
@@ -1,71 +1,63 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿//using System;
+//using System.Net;
+//using System.Net.Sockets;
+//using System.Text;
 
-namespace s_init { 
-    internal class ServerInit
-    {
-        private static TcpListener _server;
-        private static StreamWriter _writer;
-        private static IPEndPoint _ipEndPoint;
+//namespace s_init { 
+//    internal class ServerInit
+//    {
+//        private static TcpListener _server;
+//        private static StreamWriter _writer;
+//        private static IPEndPoint _ipEndPoint;
 
-        private static bool _exec = true;
-        protected static string valJson = "";
+//        private static bool _exec = true;
+//        protected static string valJson = "";
 
-	    public ServerInit()
-	    {
-            StartServer();
-	    }
+//	    public ServerInit()
+//	    {
+//            StartServer();
+//	    }
 
-        public ServerInit(bool _exec, string valJson)
-        {
-            this._exec = _exec;
-            this.valJson = valJson;
-        }
+//        public ServerInit(bool _exec, string valJson)
+//        {
+//            this._exec = _exec;
+//            this.valJson = valJson;
+//        }
 
-        internal async void StartServer(string vJson)
-        {
-            try
-            {
-                _ipEndPoint = new IPEndPoint(IPAddress.Any, 1919);
-                _server = new(_ipEndPoint);
-                _server.Start();
-                Console.WriteLine("The server has started at 1919");
-                try
-                {
-                    TcpClient handler = await _server.AcceptTcpClientAsync();
-                    Console.WriteLine("Client connected");
-                    _writer = new StreamWriter(handler.GetStream(), Encoding.UTF8)
-                    {
-                        AutoFlush = true
-                };
-                    Thread serverThread = new Thread(
-                        TransmitData
-                        /* TcpClient client = _server.AcceptTcpClient();*/
-                        );
-                    serverThread.Start();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.ToString());
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-        }
+//        internal async void StartServer(string vJson)
+//        {
+//            try
+//            {
+//                _ipEndPoint = new IPEndPoint(IPAddress.Any, 1919);
+//                _server = new(_ipEndPoint);
+//                _server.Start();
+//                Console.WriteLine("The server has started at 1919");
+//                try
+//                {
+//                    TcpClient handler = await _server.AcceptTcpClientAsync();
+//                    Console.WriteLine("Client connected");
+//                    _writer = new StreamWriter(handler.GetStream(), Encoding.UTF8)
+//                    {
+//                        AutoFlush = true
+//                };
+//                    Thread serverThread = new Thread(
+//                        TransmitData
+//                        /* TcpClient client = _server.AcceptTcpClient();*/
+//                        );
+//                    serverThread.Start();
+//                }
+//                catch (Exception ex)
+//                {
+//                    Console.WriteLine(ex.ToString());
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.ToString());
+//            }
+//        }
 
-        static void TransmitData()
-        {
-            while (_exec)
-            {
-                _writer.WriteLine(valJson);
-                Console.WriteLine("Sensor Data sent!");
-                Thread.sleep(1000);
-            }
-        }
+        
 
-    }
-}
+//    }
+//}
--- TransmitData.cs
+++ TransmitData.cs
@@ -1,20 +1,20 @@
-﻿using System;
+﻿//using System;
 
-using s_init;
+////using s_init;
 
-public class TransmitData
-{
-	public TransmitData()
-	{
-	}
+//public class TransmitData
+//{
+//	public TransmitData()
+//	{
+//	}
 
-    static void TransmitData()
-    {
-        while (_exec)
-        {
-            _writer.WriteLine(valJson);
-            Console.WriteLine("Sensor Data sent!");
-            Thread.sleep(1000);
-        }
-    }
-}
+//    static void TData()
+//    {
+//        while (_exec)
+//        {
+//            _writer.WriteLine(valJson);
+//            Console.WriteLine("Sensor Data sent!");
+//            Thread.sleep(1000);
+//        }
+//    }
+//}
