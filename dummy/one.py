b7c3a46baee49f6f1f26484d9234a491e49fc334
AeR0 <prithvichavan56110@gmail.com>
2025-08-20 20:44:04 +0530

    Day 1 done!

---
+++ dummy/one.py
@@ -0,0 +1,30 @@
+import sys
+from time import sleep
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PySide6.QtCore import QTimer
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle("Dummy Front")
+
+label = QLabel("This is the label")
+button = QPushButton("Click Me!")
+
+
+def buttonClick():
+    label.setText("Click was registered!")
+    QTimer.singleShot(1000, lambda: label.setText("This is the label"))
+    # label.setText("This is the label")
+
+
+button.clicked.connect(buttonClick)
+
+encompass = QVBoxLayout()
+encompass.addWidget(label)
+encompass.addWidget(button)
+window.setLayout(encompass)
+
+window.show()
+
+sys.exit(app.exec())
